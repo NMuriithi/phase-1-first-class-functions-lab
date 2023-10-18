996a0d9ddde77c319f29b06a0b55f052ed439bda
Nicholus <nicholus.muriithi@student.moringaschool.com>
2023-10-18 10:56:42 -0700

    defined various functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 1);
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+}
+  function createFareMultiplier(integer) {
+  return function(fare) {
+    return fare * integer;
+  }
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(drivers ,returnlastTwoDrivers){
+ return returnLastTwoDrivers(drivers);
+}
+
